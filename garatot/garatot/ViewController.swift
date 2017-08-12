8ffdec7fba8556fe2aa62f1351403fd732f1adbe
VietLH <vietlh@VietLHs-MacBook-Pro.local>
2017-08-12 21:55:56 +0700

    init project

---
+++ garatot/garatot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garatot
+//
+//  Created by VietLH on 8/12/17.
+//  Copyright © 2017 VietLH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
